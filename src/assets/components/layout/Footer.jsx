43be5629c663749b5300b03ab610ee1b942d7d6e
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-05-24 09:02:42 -0300

    style: add margin
    inside of elements footer

--- src/assets/components/layout/Footer.jsx
+++ src/assets/components/layout/Footer.jsx
@@ -8,18 +8,25 @@ Footer.propTypes = {
 function Footer(props) {
   return (
     <footer>
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 py-4 text-center">
         
-        <header>
-          <h1>header</h1>
+        <header className='mb-2 mt-8'>
+          <h1 className='text-6xl text-pink-300 font-sans'>we're</h1>
+          <h2 className=' text-6xl font-sans'>coming </h2>
+          <h3 className='text-6xl font-sans'>soon</h3>
         </header>
 
-        <section>
-          <h2>section</h2>
+        <section className='mb-8'>
+          <p className='w-4/5 text-justify m-auto'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis cum aliquid autem ipsum labore dolor porro vel iusto architecto sapiente reprehenderit consequuntur provident,Velit quos corrupti consectetur libero et officia corporis ab quae, consectetur?</p>
         </section>
 
-        <footer>
-          <h3>footer</h3>
+        <footer className='flex w-4/5 mx-auto '>
+            <input type="email" placeholder='email address' />
+            <div className='bg-pink-300'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16">
+              <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
+            </svg>
+            </div>
         </footer>
 
       </div>
