c6e05b92518d3611f74799c2056ed1f7cf6bf411
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-06-01 07:48:52 -0300

    fix: remove className in svg
    class apparearing twice

--- src/assets/components/layout/Footer.jsx
+++ src/assets/components/layout/Footer.jsx
@@ -23,7 +23,7 @@ function Footer(props) {
         <footer className='flex w-4/5 mx-auto relative'>
             <input className='w-full  border-pink-700 border-2 rounded-lg py-0.5 placeholder:pl-4 relative' type="email" placeholder='email address' />
             <div className='h-full bg-pink-300 rounded-lg absolute right-0'>
-              <svg className='' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16">
                 <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
               </svg>
             </div>
