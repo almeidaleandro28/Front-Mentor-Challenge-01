eb26aaa2e0653f6699587e8ff0ebbf41bba3d35f
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-06-05 11:40:09 -0300

    style: btn
    add padding and effect hover and active

--- src/assets/components/layout/Footer.jsx
+++ src/assets/components/layout/Footer.jsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-Footer.propTypes = {
-  
-};
 
 function Footer(props) {
   return (
@@ -26,17 +20,22 @@ function Footer(props) {
         </section>
 
         <footer className='flex w-4/5 mx-auto relative'>
-            <input className='w-full  border-pink-700 border-2 rounded-xl relative py-2 placeholder:text-pink-400 placeholder:pl-2'
-               type="email" 
-               placeholder='email address' />
-            <div className='h-full bg-pink-300 rounded-xl absolute right-0 flex items-center'>
+            <input className='w-full border-pink-300 border-2 rounded-xl relative py-2 
+              placeholder:text-pink-400 placeholder:pl-2
+              active:border-red-600 hover:border-red-600'
+              type="email" 
+              placeholder='email address' />
+            <div className='h-full bg-pink-400 rounded-xl absolute right-0 flex items-center pl-8 pr-8 
+              hover:scale-x-150 hover:bg-pink-300 active:scale-x-150 active:bg-pink-300'>
               <svg xmlns="http://www.w3.org/2000/svg" 
                 width="16" 
                 height="16" 
                 fill="currentColor" 
                 className="bi bi-caret-right" 
                 viewBox="0 0 16 16">
-                <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
+                <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1
+                 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 
+                 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
               </svg>
             </div>
         </footer>
--- src/assets/components/layout/Header.jsx
+++ src/assets/components/layout/Header.jsx
@@ -8,7 +8,9 @@ Header.propTypes = {
 function Header(props) {
   return (
     <header className='bg-pink-200 pl-2 pt-3 pb-5 md:col-span-1 md:row-span-1'>
-      <Logo />
+      <div className="container mx-auto px-4 py-4">
+        <Logo />
+      </div>
     </header>
   );
 }
